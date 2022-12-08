144186f291a820daa0a6187294b8cfa6cac9187f
Oscar Söderlund <oscar@nebulr.group>
2022-12-08 12:00:51 +0100

    Updated container setup

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+## Usage
+## ./init_local.sh WORKSPACE_FOLDER_PATH
+PWD=$(dirname $0)
+source $PWD/secrets.env
+
+echo "Updating and installing tools"
 apt-get update
-apt-get -y install less vim curl jq
+apt-get -y install vim less curl jq
 
+# Git tool config
+echo "Configuring GIT"
 git config --global core.editor "vim"
+git config --global user.email $GIT_EMAIL
+git config --global user.name $GIT_NAME
 
+# Useful dependecies
 npm install -g @nestjs/cli@8
+
+echo "Done dev specific initializing, running workspace_folder/init.sh..."
+. $1/init.sh
